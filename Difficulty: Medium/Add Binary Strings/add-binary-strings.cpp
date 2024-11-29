fe7dbc735058140ef75468cdd781cd51b72858a4
Ayushi Singhal <92862556+AyushiSinghal14@users.noreply.github.com>
2024-11-29 22:01:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add Binary Strings/add-binary-strings.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    string addBinary(string& s1, string& s2) {
+        string ans = "";  // To store the binary sum
+        int i = s1.size() - 1;  // Pointer for `s1`
+        int j = s2.size() - 1;  // Pointer for `s2`
+        int carry = 0;  // Initialize carry
+
+        // Process both strings until all bits are covered
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int count1s = carry;  // Start with carry
+
+            // Add 1 if the current bit of `s1` is '1'
+            if (i >= 0 && s1[i] == '1') count1s++;
+
+            // Add 1 if the current bit of `s2` is '1'
+            if (j >= 0 && s2[j] == '1') count1s++;
+
+            // Append the result bit to `ans`
+            ans += (count1s % 2 == 0 ? '0' : '1');
+
+            // Update carry
+            carry = count1s / 2;
+
+            // Move pointers to the next bit
+            i--;
+            j--;
+        }
+        // Reverse the result string to correct its order
+        reverse(ans.begin(), ans.end());
+        // To avoid leading zeros
+        i=0;
+        while(ans[i]=='0'){
+            ans.erase(0,1);
+         }
+        return ans;
+        }
+   };
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string A, B;
+        cin >> A >> B;
+        Solution ob;
+        cout << ob.addBinary(A, B);
+        cout << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
