981ad6abd6ee82207ec7794d90410423ad9d4297
Ayushi Singhal <92862556+AyushiSinghal14@users.noreply.github.com>
2025-09-08 12:40:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.py
@@ -0,0 +1,21 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def mergeSort(self, head):
+        initAns=[]
+        new=head
+        while new:
+            initAns.append(new.data)
+            new=new.next
+        initAns.sort()    
+        current=head
+        for i in initAns:
+            current.data=i
+            current=current.next
+        return head
+        
